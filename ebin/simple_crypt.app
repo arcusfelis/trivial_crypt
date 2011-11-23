55c70ae2dace616f4c6bb606778c184fdbfffcee
Uvarov Michael <freeakk@gmail.com>
2011-11-23 09:05:39 +0300

    Add files.

---
+++ ebin/simple_crypt.app
@@ -0,0 +1,7 @@
+{application,simple_crypt,
+             [{description,[]},
+              {vsn,"1"},
+              {registered,[]},
+              {applications,[kernel,stdlib]},
+              {env,[]},
+              {modules,[cesar]}]}.
---
+++ src/cesar.erl
@@ -0,0 +1,24 @@
+-module(cesar).
+-export([encode/2, decode/2
+	]).
+
+encode(S, N) -> 
+	encode(S, N, []).
+
+encode([H|T], N, Acc) 
+	when H>=$a andalso H=<$z ->
+	Diff = $z-$a,
+	NewH = ((H - $a + N) rem Diff) + $a,
+	encode(T, N, [NewH|Acc]);
+encode([H|T], N, Acc) 
+	when H>=$A andalso H=<$Z ->
+	Diff = $Z-$A,
+	NewH = ((H - $A + N) rem Diff) + $A,
+	encode(T, N, [NewH|Acc]);
+encode([H|T], N, Acc) ->
+	encode(T, N, [H|Acc]);
+encode([], _N, Acc) ->
+	lists:reverse(Acc).
+
+decode(S, N) -> 
+	encode(S, -N, []).
