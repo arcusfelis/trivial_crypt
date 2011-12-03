e31af5b3a8014101588851f7687e33c51d5ff481
Uvarov Michael <freeakk@gmail.com>
2011-12-03 20:38:13 +0400

    move files.

---
+++ ebin/trivial_crypt.app
@@ -0,0 +1,7 @@
+{application,trivial_crypt,
+             [{description,[]},
+              {vsn,"1"},
+              {registered,[]},
+              {applications,[kernel,stdlib]},
+              {env,[]},
+              {modules,[caesar,cesar]}]}.
--- start-dev.sh
+++ start-dev.sh
@@ -6,7 +6,7 @@ make
 # NOTE: mustache templates need \ because they are not awesome.
 exec erl -pa $PWD/ebin edit $PWD/deps/*/ebin \
     -boot start_sasl \
-    -sname simple_crypt \
+    -sname trivial_crypt \
 #   -s i18n_nif init \
 #   -s reloader
 
