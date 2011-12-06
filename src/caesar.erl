292afe57775978a5c0c216de3f505ffcde86d3d0
Uvarov Michael <freeakk@gmail.com>
2011-12-06 12:59:00 +0400

    Fix negative values.

--- src/caesar.erl
+++ src/caesar.erl
@@ -1,22 +1,23 @@
 %%% The realization of [Caesar cipher](http://en.wikipedia.org/wiki/Caesar_cipher)
 
 -module(caesar).
--export([encode/2, decode/2
-	]).
+-export([encode/2, decode/2,
+	all_variants/1]).
 
 
 encode(S, N) -> 
 	encode(S, N, []).
 
+%% +Diff fixes negative values
 encode([H|T], N, Acc) 
 	when H>=$a andalso H=<$z ->
-	Diff = $z-$a,
-	NewH = ((H - $a + N) rem Diff) + $a,
+	Diff = $z-$a+1,
+	NewH = ((H - $a + N + Diff) rem Diff) + $a,
 	encode(T, N, [NewH|Acc]);
 encode([H|T], N, Acc) 
 	when H>=$A andalso H=<$Z ->
-	Diff = $Z-$A,
-	NewH = ((H - $A + N) rem Diff) + $A,
+	Diff = $Z-$A+1,
+	NewH = ((H - $A + N + Diff) rem Diff) + $A,
 	encode(T, N, [NewH|Acc]);
 encode([H|T], N, Acc) ->
 	encode(T, N, [H|Acc]);
@@ -24,4 +25,9 @@ encode([], _N, Acc) ->
 	lists:reverse(Acc).
 
 decode(S, N) -> 
-	encode(S, -N, []).
+	encode(S, -N).
+
+all_variants(S) ->
+    lists:map(fun(N) ->
+            decode(S, N)
+        end, lists:seq(0,25)).
